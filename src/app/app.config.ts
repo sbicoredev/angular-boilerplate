1fbe2e3ed65a3e2d71064c53698714c39d325fe3
Sajid <sbicoredev@gmail.com>
2026-05-09 10:39:28 +0600

    style: format file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,19 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  type ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+} from "@angular/core";
+import {
+  provideClientHydration,
+  withEventReplay,
+} from "@angular/platform-browser";
+import { provideRouter } from "@angular/router";
 
-import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+  ],
 };
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
+import { join } from "node:path";
+
 import {
   AngularNodeAppEngine,
   createNodeRequestHandler,
   isMainModule,
   writeResponseToNodeResponse,
-} from '@angular/ssr/node';
-import express from 'express';
-import { join } from 'node:path';
+} from "@angular/ssr/node";
+import express from "express";
 
-const browserDistFolder = join(import.meta.dirname, '../browser');
+const browserDistFolder = join(import.meta.dirname, "../browser");
 
 const app = express();
 const angularApp = new AngularNodeAppEngine();
@@ -29,10 +30,10 @@ const angularApp = new AngularNodeAppEngine();
  */
 app.use(
   express.static(browserDistFolder, {
-    maxAge: '1y',
+    maxAge: "1y",
     index: false,
     redirect: false,
-  }),
+  })
 );
 
 /**
@@ -42,7 +43,7 @@ app.use((req, res, next) => {
   angularApp
     .handle(req)
     .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
+      response ? writeResponseToNodeResponse(response, res) : next()
     )
     .catch(next);
 });
@@ -51,8 +52,8 @@ app.use((req, res, next) => {
  * Start the server if this module is the main entry point, or it is ran via PM2.
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
-if (isMainModule(import.meta.url) || process.env['pm_id']) {
-  const port = process.env['PORT'] || 4000;
+if (isMainModule(import.meta.url) || process.env["pm_id"]) {
+  const port = process.env["PORT"] || 4000;
   app.listen(port, (error) => {
     if (error) {
       throw error;
